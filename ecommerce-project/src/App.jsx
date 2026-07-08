3250a91c17d6a914eeba0206e251c7886483cff1
Buddizansal <buddizansal@gmail.com>
2026-07-08 13:46:46 +0530

    seperate into smaller components

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,9 +1,9 @@
 import axios from 'axios';
 import {Routes,Route} from 'react-router';
 import { useState, useEffect } from 'react';
-import { HomePage } from './pages/HomePage'
+import { HomePage } from './pages/home/HomePage'
 import { CheckoutPage } from './pages/checkout/CheckoutPage';
-import { OrdersPage } from './pages/OrdersPage';
+import { OrdersPage } from './pages/orders/OrdersPage';
 import { TrackPackage } from './pages/TrackPackage';
 import { NotFound } from './pages/Not Found';
 import './App.css'
---
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+import {useEffect,useState} from "react";
+import { Header } from "../../components/Header.jsx";
+import "./HomePage.css";
+
+
+import { ProductsGrid } from "./ProductsGrid";
+export function HomePage({cart}) {
+    const [products, setProducts] = useState([]);
+    
+    useEffect(()=>{
+        axios.get('http://localhost:3000/api/products')
+            .then((response)=>{
+            setProducts(response.data)
+            });
+        
+    },[])
+    
+  return (
+    <>
+      <title>Ecommerce Project</title>
+
+      <link rel="icon" type="image/svg+xml" href="/home-favicon.png" />
+
+      <Header cart={cart} />
+      <div className="home-page">
+        <ProductsGrid products={products} />
+      </div>
+    </>
+  );
+}
